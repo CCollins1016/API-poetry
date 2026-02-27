8bdfcc6663f40d95084595aa20fb53a90408a85f
CCollins1016 <ccollins@bsd220.org>
2026-02-27 10:36:37 -0600

    Initial Commit

---
+++ API poetry/API_poetryApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_poetryApp.swift
+//  API poetry
+//
+//  Created by Christian Collins on 2/27/26.
+//
+
+import SwiftUI
+
+@main
+struct API_poetryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
